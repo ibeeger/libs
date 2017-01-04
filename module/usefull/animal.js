e6f8de34576adf73623f78003bf84434664ad8d8
ibeeger <cxh80301983@126.com>
2017-01-04 20:36:40 +0800

    updata anims

--- module/usefull/animal.js
+++ module/usefull/animal.js
@@ -2,7 +2,7 @@
  * @Author: ibeeger
  * @Date:   2017-01-04 16:54:14
  * @Last Modified by:   ibeeger
- * @Last Modified time: 2017-01-04 20:03:51
+ * @Last Modified time: 2017-01-04 20:33:31
  */
 
 'use strict';
@@ -15,8 +15,19 @@ const exec = require('child_process').exec;
 const pinyin = require("pinyin");
 var num = 0;
 
+var dbconf = require("./dbconfig.js");
+var MongoClient = require('mongodb').MongoClient,
+	db;
+var addr = dbconf.db;
 
-console.log(pinyin)
+MongoClient.connect(addr, function(err, sdb) {
+	assert.equal(err, null);
+	db = sdb.collection("anims");
+	obj.url = names[num];
+})
+
+
+
 
 var obj = {};
 var url = "http://www.aidongwu.net/dongwu/";
@@ -31,17 +42,17 @@ Object.defineProperty(obj, "url", {
 			scripts: ["http://libs.willclass.com/libs/jquery/1.11.1/jquery.min.js"],
 			done: function(err, window) {
 				var $ = window.$;
-				var anim = {};
-				console.log("===============");
+				console.log("===============================================================================");
 				let index = $(".tag-animal>.keshu").index();
 				let img = $(".tag-img").attr("src"); //图片地址
 				let fileType = img.split("/")[img.split("/").length - 1].split(".")[1]; //图片后缀名
-				let englistName = $(".tag-animal>.keshu>dl:eq(1)").find(":last-child").text();
+				console.log($(".tag-animal>.keshu>dl").length)
+				let englistName = $(".tag-animal>.keshu>dl").length>1 ?  $(".tag-animal>.keshu>dl:eq(1)").find(":last-child").text().toLocaleLowerCase().split("；")[0] : "";
 
 				if (!englistName || !englistName.match(/[a-zA-Z]/g)) {
 					englistName = pinyin(name, {
 						style: pinyin.STYLE_NORMAL
-					}).join(",");
+					}).join("").toLocaleLowerCase();
 
 				};
 
@@ -52,11 +63,27 @@ Object.defineProperty(obj, "url", {
 						desc += $(".tag-animal>p").eq(i).text() + "\n";
 					}
 				};
+				console.log(num+"/"+names.length);
 				console.log("名称" + name);
 				console.log("英文名" + englistName);
 				console.log("图片类型" + fileType)
 				console.log("描述:" + desc);
 				console.log("图片地址:" + img);
+
+				let anim = {
+					name: name,
+					ename: englistName,
+					desc: desc,
+					img: filename + "." + fileType
+				};
+
+				// db.insert(anim, function(err) {
+				// 	if (err) {
+				// 		process.exit(0)
+				// 	}
+				// 	console.log("数据储存" + err);
+				// })
+
 				if (img) {
 					exec("wget " + img + " -O " + __dirname + "/anims/" + filename + "." + fileType, function() {
 						if (arguments[0]) {
@@ -65,10 +92,14 @@ Object.defineProperty(obj, "url", {
 						}
 					})
 				};
-				num++;
-				do {
-					obj.url = names[num];
-				} while (num > names.length)
+				setTimeout(function() {
+					num++;
+
+					do {
+						obj.url = names[num];
+					} while (num > names.length)
+				}, 2500)
+
 
 			}
 		};
@@ -81,6 +112,3 @@ Object.defineProperty(obj, "url", {
 	enumerable: true,
 	configurable: true
 });
-
-
-obj.url = names[num];
