f30511c540a79f95ccb9956b91d9097eacf047eb
ibeeger <cxh80301983@126.com>
2017-03-29 17:22:54 +0800

    updata

--- module/dist/http_client.js
+++ module/dist/http_client.js
@@ -2,7 +2,7 @@
  * @Author: willclass
  * @Date:   2015-10-28 14:41:09
  * @Last Modified by:   ibeeger
- * @Last Modified time: 2017-01-19 18:17:23
+ * @Last Modified time: 2017-03-29 16:53:09
  */
 
 'use strict';
@@ -18,22 +18,26 @@ var client = {
 	post: function(url, data, callback) {
 		var _data = JSON.stringify(data),
 			_datalth = Buffer.byteLength(_data, 'utf8');
-
 		var _options = {
 			hostname: host,
 			port: port,
 			path: url,
 			method: method,
 			headers: {
-				'User-Agent': 'iPhone 6s',
+				"HT-Login-Token":"TWd2M2NwL3FNdG9GWHp2SU81MzQ3Zz09",
+				"HT-Platform-Type":"android",
+				"HT-Client-Version":"3.5.0",
+				"HT-Version-Code":30500,
+				"HT-Device-id":353347063280671,
+				// 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
 				'Content-Type': type,
-				'Content-Length': _datalth,
-				// 'Cookie': cookie
+				// 'Content-Length': _datalth,
+				// 'Cookie': "HT_bench_01=MlZJMU9xcUFoTFFGWHp2SU81MzQ3Zz09|0; HT_SSO_COOKIE=U3VpSmEvazdndjBGWHp2SU81MzQ3Zz09|11|1490764828278|31008c327808a066b625d913f66de9b8; JSESSIONID=FC0B0772190EE9B0EEA21BE637F06F54; sid=1490771544067"
 			}
 		};
 
 		var _req = http.request(_options, function(res) {
-			console.log(res.statusCode);
+			// console.log("statecode:"+res.statusCode);
 			var str = "";
 			res.setEncoding('utf8');
 			res.on("data",function(body){
--- module/usefull/requesturl.js
+++ module/usefull/requesturl.js
@@ -1,15 +1,24 @@
 /* 
 * @Author: willclass
 * @Date:   2015-11-09 14:19:58
-* @Last Modified by:   willclass
-* @Last Modified time: 2016-01-09 15:39:09
+* @Last Modified by:   ibeeger
+* @Last Modified time: 2017-03-29 17:00:28
 */
 
 'use strict';
 
 var clt = require("../dist/http_client.js");
 
-clt.setHost("localhost");
-clt.setPort("8798");
+// clt.setMethod("GET");
+clt.setHost("student.huitong.com");
+// clt.setHost("t.willclass.com");
+var i =0;
+while(i<1000){
+	
+		clt.post("/api/wireless/v340/task/schoolwork/index",{},function(arg){
+				console.log(arg)
+		})
+		i++
 
+}
   
